1817622839804da6ae8f8d169a17e4db421baaa8
Tim Voronkin <voronkintim@gmail.com>
2026-04-05 19:09:33 +0200

    bg opt

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ function loadTexture(url, unit, isMain) {
     const texture = gl.createTexture();
     gl.activeTexture(gl.TEXTURE0 + unit);
     gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([0,0,0,255]));
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([0,0,0,0]));
     
     const image = new Image();
     image.src = url;
